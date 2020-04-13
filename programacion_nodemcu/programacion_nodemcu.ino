6095c8e413b8ffe16ecc70d617cbb4a0f241362b
JulianRamirezJ <julianarj39000@gmail.com>
2020-04-12 20:20:58 -0500

    Envio de datos a thingspeak

--- programacion_nodemcu/programacion_nodemcu.ino
+++ programacion_nodemcu/programacion_nodemcu.ino
@@ -1,3 +1,5 @@
+#include <ThingSpeak.h>
+#include <ESP8266WiFi.h> // Incluye la librería ESP8266wifi
 
 #define timeSeconds 1
 //LCD 16*2 I2C Display libraries
@@ -5,18 +7,16 @@
 #include <LiquidCrystal_I2C.h>    
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 //LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-#include "ThingSpeak.h" // Incluye la librería ThingSpeak
-#include <ESP8266WiFi.h> // Incluye la librería ESP8266wifi
 
 //Varibles de conexion al servidor
 // ----- Cambiar XXXXX por el nombre de la red wifi o datos a la que se va a conectar
-char ssid[] = "XXXXX"; // Nombre del WiFi (nombre del router)
+char ssid[] = "UNE_HFC_7BB0"; // Nombre del WiFi (nombre del router)
 // ----- Cambiar YYYYY por la contraseña de la red wifi o datos a la que se va a conectar 
-char pass[] = "YYYYY"; // WiFi router password
+char pass[] = "AAA0BF87"; // WiFi router password
 // ----- Cambiar 123456 por el ID de su canal en ThingSpeak
-unsigned long myChannelNumber = 123456; // Thingspeak número de canal
+unsigned long myChannelNumber = 1004093; // Thingspeak número de canal
 // ----- Cambiar ZZZZZZZZZZ por la API Key de su canal en ThingSpeak
-const char * myWriteAPIKey = "ZZZZZZZZZZ"; // ThingSpeak write API Key
+const char * myWriteAPIKey = "9O4JAFFUIFE01Y8O"; // ThingSpeak write API Key
 
 int status = WL_IDLE_STATUS; 
 WiFiClient client;
@@ -29,8 +29,10 @@ float factor_conversion=7.11;
 float caudal=0;
 int tiempoPrSeg = 0;
 int tiempoPrMin = 0;
+int tiempoPrH = 0;
 int tiempoFnMin = 0;
 int tiempoFnSeg = 0;
+int tiempoFnH = 0;
 // Timer: Auxiliary variables
 unsigned long now = millis();
 unsigned long lastTrigger = 0;
@@ -47,7 +49,6 @@ ICACHE_RAM_ATTR void detectsMovement() {
   pulsos=pulsos+1;
   tiempo = tiempo + 0.001;
 }
-
 void setup() {
     WiFi.begin(ssid, pass); // Inicia la conexion WiFi 
     ThingSpeak.begin(client); // Inicia ThingSpeak
@@ -60,8 +61,7 @@ void setup() {
   // PIR Motion Sensor mode INPUT_PULLUP
   pinMode(motionSensor, INPUT_PULLUP);
   // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
-
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING); 
   //Conexion al servidor
    Serial.println();
    Serial.print("Conectando a ");
@@ -81,9 +81,9 @@ void setup() {
   void loop() {
      estado = digitalRead(pinInfr);
     Serial.println(estado);
-  if(estado == 1){
-    delay(1500);
-    do{
+   if(estado == 1){
+     delay(1500);
+     do{
       delay(150);
        proceso();
        estado = digitalRead(pinInfr);
@@ -95,39 +95,33 @@ void setup() {
   }
   void proceso(){
       // Current time
-  now = millis();
-  // Turn off the LED after the number of seconds defined in the timeSeconds variable
-  if(now - lastTrigger > (timeSeconds*1000)) {
-    lastTrigger=now;
-    guardar= pulsos;
-    pulsos=0;
-    caudal = guardar/factor_conversion;
-    volumen=volumen+caudal/60;
+   now = millis();
+   // Turn off the LED after the number of seconds defined in the timeSeconds variable
+   if(now - lastTrigger > (timeSeconds*1000)) {
+     lastTrigger=now;
+     guardar= pulsos;
+     pulsos=0;
+     caudal = guardar/factor_conversion;
+     volumen=volumen+caudal/60;
 
     if(caudal != 0){
-      tiempoD();
-     /* Serial.print("Caudal: ");
-      Serial.print(caudal);
-      Serial.println(" L/m");
-      Serial.print("Volumen: ");
-      Serial.print(volumen);
-      Serial.println(" L");*/
-      char tpM[10];
-      sprintf(tpM,"%02d%c%02d%s", tiempoPrMin,':',tiempoPrSeg," Min");
-      Serial.println(tpM);
-      //Use the display to print vol & time
-      lcd.setCursor(0,0); // Inicio del cursor
-      lcd.print("L. GASTO : ");
-      lcd.print(volumen);
-      lcd.setCursor(0,1); 
-      lcd.print("TIEMPO : ");
-      lcd.print(tpM);
-      if(volumen > 10){
-        digitalWrite(pinbuzer,HIGH);
-      }
-      else{
-        digitalWrite(pinbuzer,LOW);
-      }
+       tiempoD();
+       char tpM[12];
+       sprintf(tpM,"%02d%c%02d%c%02d",tiempoPrH,':', tiempoPrMin,':',tiempoPrSeg);
+       Serial.println(tpM);
+       //Use the display to print vol & time
+       lcd.setCursor(0,0); // Inicio del cursor
+       lcd.print("L. GASTO : ");
+       lcd.print(volumen);
+       lcd.setCursor(0,1); 
+       lcd.print("TIEMPO:");
+       lcd.print(tpM);
+       if(volumen > 10){
+         digitalWrite(pinbuzer,HIGH);
+       }
+       else{
+         digitalWrite(pinbuzer,LOW);
+       }
     }
    }
   }
@@ -137,22 +131,28 @@ void setup() {
        tiempoPrSeg = 0;
        tiempoPrMin++;
     }
+    if(tiempoPrMin == 60){
+      tiempoPrMin = 0;
+      tiempoPrH++;
+    }
     delay(1000);
   }
- /* int sensorIr(){
-    int  estado = digitalRead(pinInfr);
-     return estado;
-  }*/
   void reinicio(){
     tiempoPrSeg = 0;
     tiempoPrMin = 0;
+    tiempoPrH = 0;
     lcd.clear();
   }
   void enviar(){
     Serial.print("Ultimo dato enviado =");
       char tpM[10];
-      sprintf(tpM,"%02d%c%02d%s\n%s%i", tiempoPrMin,':',tiempoPrSeg," Min");
-      Serial.println(tpM); 
+      sprintf(tpM,"%02d%c%02d%c%02d",tiempoPrH,':', tiempoPrMin,':',tiempoPrSeg);
+        ThingSpeak.setField(1,volumen);
+        ThingSpeak.setField(2,tpM );
+       // Escribe todos los campos a la vez.
+      ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+      Serial.println(tpM);
+      Serial.println(volumen); 
       delay(2000);
   }
 
